899fa7d87a7669c69e3f86873a450e4bdfed1fa7
Egor-Kozh <kozhevnikovegor55@gmail.com>
2024-12-13 19:16:10 +0600

    fix_modal

--- budss/index.js
+++ budss/index.js
@@ -21,6 +21,21 @@ modal_close.addEventListener("click", function(e){
 function Close_modal(){
     const modal_open = document.querySelector(".modal.active")
     modal_open.classList.remove("active")    
+
+    const wrong_text = document.querySelectorAll(".wrong")
+    for(let i = 0; i < wrong_text.length; i++){
+        wrong_text[i].remove()
+    }
+
+    const wrong_input = document.querySelectorAll(".wrong_input")
+    for(let i = 0; i < wrong_input.length; i++){
+        wrong_input[i].style.borderColor = "rgba(241, 241, 241, 1)";
+        wrong_input[i].classList.remove("wrong_input")
+    }   
+}
+
+function ClearInput(event){
+
 }
 
 
@@ -42,7 +57,6 @@ modal_submit.addEventListener("click", function(e){
         userList.show_all_users()
         
         Close_modal()
-        debugger
     }
 })
 function CheckInputValue(bool){
@@ -53,13 +67,58 @@ function CheckInputValue(bool){
         for(let i = 0; i < input_required.length; i++){
             if(!input_required[i].value){
                 flag = false
+
+                if(!document.querySelector(`.wrong_${input_required[i].name}`)){
+                    const wrong_input = document.getElementById(`${input_required[i].name}`)
+                    wrong_input.style.borderColor = "rgba(236, 18, 17, 1)";
+                    wrong_input.classList.add("wrong_input")
+                    wrong_input.addEventListener("click" , function Clear(e){
+                        console.log("clear")
+                        wrong_input.classList.remove(".wrong_input")
+                        wrong_input.style.borderColor = "rgba(241, 241, 241, 1)";
+                        const wrong = document.querySelector(`.wrong_${wrong_input.id}`)
+                        wrong.remove()
+
+                        if(document.querySelectorAll(".wrong").length == 1){
+                            document.querySelector(".wrong_modal").remove()
+                        }
+
+                        wrong_input.removeEventListener("click", Clear)
+                    })
+
+                    const wrong_text = document.createElement("p")
+                    wrong_text.classList.add(`wrong_${input_required[i].name}`)
+                    wrong_text.classList.add(`wrong`)
+                    wrong_text.innerHTML = "This field is required."
+                    wrong_text.style.color = "rgba(236, 18, 17, 1)"
+                    wrong_text.style.fontSize = "12px"
+                    wrong_text.style.fontFamily = "Outfit_Regular"
+                    wrong_text.style.height = "20px"
+                    wrong_text.style.width = "fit-content"
+
+                    const wrong_field = wrong_input.parentElement
+                    wrong_field.appendChild(wrong_text)
+                }
             }
+        }
+        if(!flag & !document.querySelector(".wrong_modal")){
+            const wrong_text = document.createElement("p")
+            wrong_text.classList.add("wrong_modal")
+            wrong_text.classList.add("wrong")
+            wrong_text.innerHTML = "Please fill in all required fields"
+            wrong_text.style.color = "rgba(236, 18, 17, 1)"
+            wrong_text.style.fontSize = "16px"
+            wrong_text.style.fontFamily = "Outfit_Regular"
+            wrong_text.style.height = "20px"
+            wrong_text.style.width = "fit-content"
+
+            const wrong = document.querySelector(".modal_form")
+            wrong.appendChild(wrong_text)
         }
     }
 
     return flag
 }
-
 
 class User{
     constructor({name, email, phone, company, website_url}){
